c20e72d0629734e6c0df9863aeeb4933357b0154
Eric <ichbincool>
2022-08-27 14:20:58 +0200

    Updated version with the chaos of Evian

--- Main.kt
+++ Main.kt
@@ -3,62 +3,73 @@ package parking
 // Creating multidimensional List
 class ParkingLot(
     var parkingNumber: Int,
-    var isEmpty: Boolean = true,
-    var carNumber: String = "",
-    var carColor: String = ""
+    var car: Car? = null
 ) {
-    fun clear(){
-        this.isEmpty = true
-        this.carNumber = ""
-        this.carColor = ""
+    var isEmpty: () -> Boolean = {car == null}
+    fun parkCar (car: Car): Boolean {
+        return if (this.isEmpty()) {
+            this.car = car
+            true
+        } else false
+
+    }
+    fun clearCar(){
+        this.car = null
     }
 }
-class ParkHouse(
-    var parkingLots: MutableList<ParkingLot>,
-    var emptyLotsList: MutableList<Boolean>
+class Car(
+    var carNumber: String = "",
+    var carColor: String = ""
 )
-fun generateListParkingLots(amount: Int): ParkHouse {
-    var parkingLots: MutableList<ParkingLot> = emptyList<ParkingLot>() as MutableList<ParkingLot>
-    var emptyLotsList: MutableList<Boolean> = emptyList<Boolean>() as MutableList<Boolean>
+class ParkHouse(
+    private val size: Int,
+    private var parkingLots: MutableList<ParkingLot> = emptyList<ParkingLot>() as MutableList<ParkingLot>
+) {
+    init {
+        for (i in 1 until  size) this.addLot(ParkingLot(i))
+    }
+    fun addLot(parkingLot: ParkingLot){
+        parkingLots.plus(parkingLot)
+    }
 
-
-    return ParkHouse(parkingLots,emptyLotsList)
-}
-
-fun parking(parkingLotOne: ParkingLot, parkingLotTwo: ParkingLot){
-        var userInput = readln().filter { it != '.' }.split(" ") as MutableList<String>
-        when(userInput[0]) {
-            "park" -> {
-                if (parkingLotOne.isEmpty){
-                    parkingLotOne.carNumber = userInput[1]
-                    parkingLotOne.carColor = userInput[2]
-                    parkingLotOne.isEmpty = false
-                    println("${parkingLotOne.carColor} car parked in spot ${parkingLotOne.parkingNumber}.")
-                } else if (parkingLotTwo.isEmpty){
-                    parkingLotTwo.carNumber = userInput[1]
-                    parkingLotTwo.carColor = userInput[2]
-                    parkingLotTwo.isEmpty = false
-                    println("${parkingLotTwo.carColor} car parked in spot ${parkingLotTwo.parkingNumber}.")
-                } 
-            }
-            "leave" -> {
-                if (userInput[1].toInt() == parkingLotOne.parkingNumber) {
-                    if (parkingLotOne.isEmpty) println("There is no car in spot ${parkingLotOne.parkingNumber}.")
-                    else {
-                        parkingLotOne.clear()
-                        println("Spot ${parkingLotOne.parkingNumber} is free.")
-                    }
-                } else if (userInput[1].toInt() == parkingLotTwo.parkingNumber) {
-                    if (parkingLotTwo.isEmpty) println("There is no car in spot ${parkingLotTwo.parkingNumber}.")
-                    else {
-                        parkingLotTwo.clear()
-                        println("Spot ${parkingLotTwo.parkingNumber} is free.")
-                    }
-                }
+    fun getEmptySpace(): Int {
+        for (i in 0 until parkingLots.size) {
+            if (parkingLots[i].isEmpty()) {
+                return i
             }
         }
+        return -1
+    }
+
+    fun parkCar(car: Car): Boolean {
+        var lotID = getEmptySpace()
+        if (lotID == -1) return false
+        return parkingLots[lotID].parkCar(car)
+    }
+    fun clearCar(lotID: Int) {
+        parkingLots[lotID].clearCar()
+    }
+
 }
 
+
+fun parking(parkingLotOne: ParkingLot, parkingLotTwo: ParkingLot){
+       Loop@while (true){
+           val userInput = readln().filter { it != '.' }.split(" ") as MutableList<String>
+            when(userInput[0]) {
+                "park" -> {
+                   continue@Loop
+                }
+                "leave" -> {
+                  continue@Loop
+                }
+                "exit" -> {
+                    break@Loop
+                }
+            }
+       }
+}
 fun main() {
+    val ParkingHouse: ParkHouse = ParkHouse(20)
 
 }
